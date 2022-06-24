9f2ef4286d1d54a915ddf99184388d3286cc316e
zoltalar <voytechp@gmail.com>
2022-06-24 14:49:09 +0200

    tweaking ui for farm reviews (removed border and padding)

--- components/FarmReviews.vue
+++ components/FarmReviews.vue
@@ -1,21 +1,10 @@
 <template>
-  <div class="reviews">
-    <h6 class="mb-3" id="reviews">{{ $t('phrases.reviews') }}</h6>
-    <review-list-item class="mb-4" :review="review" v-for="(review, i) in pagedReviews" :key="'review-list-item-' + i">
-      <template v-slot:footer>
-        <b-card-footer>
-          <small class="text-muted">
-            {{
-              $t('messages.farm_review_footer', {
-                date: shortDate(review.created_at, localeDateFormat()),
-                time: shortTime(review.created_at),
-                user: fullName(review.user, true)
-              })
-            }}
-          </small>
-        </b-card-footer>
-      </template>
-    </review-list-item>
+  <div id="reviews" class="reviews">
+    <h6 class="mb-3">{{ $t('phrases.reviews') }}</h6>
+    <div class="review-wrap" v-for="(review, i) in pagedReviews" :key="'review-' + i">
+      <review class="mb-4" :review="review" />
+      <hr v-if="i < pagedReviews.length - 1" />
+    </div>
     <b-pagination
       v-model="pagination.currentPage"
       :items="reviews"
@@ -36,7 +25,7 @@ export default {
   data: () => ({
     pagination: {
       currentPage: 1,
-      perPage: 1
+      perPage: 3
     }
   }),
   computed: {
