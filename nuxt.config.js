9c2cfd98ae93324ce850d26ee41e1facd4c6ad7d
zoltalar <voytechp@gmail.com>
2022-12-06 10:51:11 +0100

    GA4 integration

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,15 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&display=swap' }
+    ],
+    script: [
+      {
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-1NP25CZJBL',
+        async: true
+      },
+      {
+        src: 'ga.js'
+      }
     ]
   },
 
